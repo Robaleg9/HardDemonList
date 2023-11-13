da0462f5a35f24f58b64626a25323ea6bf442f01
Robaleg9 <134110589+Robaleg9@users.noreply.github.com>
2023-11-13 13:38:34 -0800

    aaaaa

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,9 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
-import ListPacks from './pages/ListPacks.js';
-import Roulette from './pages/Roulette.js'
+import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
-    { path: '/list-packs', component: ListPacks },
-    { path: '/roulette', component: Roulette }
+    { path: '/roulette', component: Roulette },
 ];
